09a640189b910fe037b6656fe3e61693175847ee
ccoffee-table <axel.olsthoorn@gmail.com>
2026-01-08 15:24:19 +1300

    test

--- nav.js
+++ nav.js
@@ -1,13 +1,14 @@
 // List your projects here - add new projects by adding to this array
 const projects = [
-    { title: "Project One", file: "project-1.html" },
+    { title: "Aquatopialien", file: "project-1.html" },
+    { title: "Sketchbook", file: "project-2.html" },
 ];
 
 // Generate and insert navigation
 const nav = `
 <nav class="sidebar">
     <div class="nav-header">
-        <h1><a href="index.html">Your Name</a></h1>
+        <h1><a href="index.html">Axel Olsthoorn</a></h1>
     </div>
     <ul class="nav-links">
         ${projects.map(p => `<li><a href="${p.file}">${p.title}</a></li>`).join('')}
